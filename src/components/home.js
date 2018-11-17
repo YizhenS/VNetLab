a1aa873887706d83ed9ca5a3beecd4c25043a827
Yizhen Shi <yizhen@Yizhens-MacBook-Air.local>
2018-11-16 22:11:23 -0500

    suck it jake

--- src/components/home.js
+++ src/components/home.js
@@ -5,6 +5,8 @@ import ConfigFile from './configFile';
 import Console from './console';
 import data from '../default.json';
 import Draggable, {DraggableCore} from 'react-draggable';
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css' // Import css
 class Home extends React.Component {
 
     constructor(){
@@ -36,6 +38,8 @@ class Home extends React.Component {
             },
             createVm:false,
             log:[],
+            file:[],
+            data:[]
            
         }
     }
@@ -50,22 +54,22 @@ class Home extends React.Component {
         }
         addVm(){
           console.log("add vm")
-          console.log(this.state.VMos)
-          Object.keys(data).forEach((key)=>{
-            if(key === "vm"){
-              data[key].push({"name":"VM:"+this.state.VMname,"os":this.state.VMos,"version":this.state.VMversion,"src":this.state.VMsrc,"eth":[]})
-            }
+          this.setState({Hubs: this.state.VMs.concat([this.state.VMname])
           })
-          this.setState({log:this.state.log.concat(["vm: "+this.state.VMname +"was created"])})
-          this.setState({VMname:"",VMos:"",VMversion:"",VMsrc:""})
         }
         addHub(){
           console.log("add hub")
-          Object.keys(data).forEach((key)=>{
-            if(key === "hub"){
-              data[key].push({"name":"HUB: "+this.state.HubName,"subnet":this.state.HubSubnet,"netmask":this.state.HubNetmast,"interface":[]})
-            }
-          })
+          this.setState({Hubs: this.state.Hubs.concat([this.state.HubName])
+              })
+          // Object.keys(data).forEach((key)=>{
+          //   if(key === "hub"){
+          //     data[key].push({"name":"HUB: "+this.state.HubName,"subnet":this.state.HubSubnet,"netmask":this.state.HubNetmast,"interface":[]})
+              
+            
+          //   }
+          // })
+          
+          
           this.setState({log:this.state.log.concat(["hub: "+this.state.HubName +"was created"])})
           this.setState({HubName:"",HubSubnet:"",HubNetmast:""})
         }
@@ -75,7 +79,7 @@ class Home extends React.Component {
         
         
           CreateVm(){
-            return this.setState({ vm: ++this.state.vm });
+            this.setState({ vm: ++this.state.vm });
           }
         
           CreateHub(){
@@ -84,49 +88,76 @@ class Home extends React.Component {
         
           
         
-          addItem(){
-            var vm = [];
-            var hub = [];
-            var all = [];
-            Object.keys(data).forEach((key)=>{
-              
-              if(key === "vm"){
-                for(var i = 0;i < data[key].length; i++){
-                  //console.log(data[key][i]["name"]);
-                  vm[i] = data[key][i]["name"];
-                  //console.log(data)
-                  
-                };
-              }else if(key === "hub"){
-                for(var i = 0;i < data[key].length; i++){
-                  //console.log(data[key][i]["name"]);
-                  hub[i] = data[key][i]["name"];
-                  //console.log(data)
-                };
+          
+
+          deleteHub(name){
+            console.log("working")
+            var hub = this.state.Hubs
+            for(var i = 0;i < hub.length; i++){
+                  console.log(hub[i])
+              if(hub[i] === name){
+                hub.splice(i,1)
+                console.log("deleted")
+                this.setState({Hubs: hub})
+                console.log(this.state.Hubs)
+               
               }
-              
-              
-            });
-            this.setState({VMs:vm,Hubs:hub})
-            console.log(data)
+            }; 
           }
 
-          deleteHub(){
-              console.log("wokring");
+          deleteVm(name){
+            var vm = this.state.VMs
+            for(var i = 0;i < vm.length; i++){
+              if(vm[i] === name){
+                vm.splice(i,1)
+                console.log("deleted")
+                this.setState({VMs: vm})
+               
+              }
+            }; 
           }
+
+          openfileConfirm = () => {
+            if (this.state.FileName === ""){
+              confirmAlert({
+                title:"File name missing",
+                message: "You need to enter a valid file name",
+                buttons:[
+                  {
+                    label:'ok'
+                  }
+                ]
+
+              })
+             }else{
+            confirmAlert({
+              title: 'Confirm to create file',
+              message: 'Are you sure creating file as: ' + this.state.FileName,
+              buttons: [
+                {
+                  label: 'Yes',
+                  onClick: () => this.CreateFile()
+                },
+                {
+                  label: 'No',
+                  onClick: () => alert('Create file canceled')
+                }
+              ]
+            })
+          }
+          };
 
     render(){
-        const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
-    const {deltaPosition, controlledPosition} = this.state;
     var VMs = this.state.VMs;
     var Hubs = this.state.Hubs;
     var log = this.state.log
+    
         if(this.state.isFile===false){
             return(
               <div>
                 <label>File name: </label>
                     <input name="FileName" value={this.state.FileName} onChange={this.handleChange}></input>
-                    <button onClick={this.CreateFile}>Newfile</button>
+                    <button onClick={this.openfileConfirm}>Newfile</button>
               </div>
             );
           }else{
@@ -141,7 +172,7 @@ class Home extends React.Component {
                   <input  name="VMversion" value={this.state.VMversion} onChange={this.handleChange}/>
                   <label>src: </label>
                   <input  name="VMsrc" value={this.state.VMsrc} onChange={this.handleChange}/>
-                  <button onClick={()=>{this.addVm();this.addItem()}}>create new vm</button>
+                  <button onClick={()=> this.addVm()}>create new vm</button>
                   <br/>
                   <label>name: </label>
                   <input name="HubName" value={this.state.HubName} onChange={this.handleChange}></input>
@@ -149,24 +180,30 @@ class Home extends React.Component {
                   <input name="HubSubnet" value={this.state.HubSubnet} onChange={this.handleChange}></input>
                   <label>netmast: </label>
                   <input name="HubNetmast" value={this.state.HubNetmast} onChange={this.handleChange}></input>
-                  <button onClick={()=>{this.addHub();this.addItem()}}>create new hub</button>
+                  <button onClick={()=>this.addHub()}>create new hub</button>
                   <br/>
               </div>
                 <div className="box" style={{height: '500px', width: '500px', position: 'relative', overflow: 'auto', padding: '0'}}>
-                <button onClick={()=>{this.deleteHub()}}>delete</button>
-                {VMs.map(function(VMs, index){
-                   return <Draggable bounds="parent"><div  className="box"><p key={ index }>{VMs}</p><button onClick={()=>{this.deleteHub()}}>delete</button></div></Draggable>;
-                 })}
-                 {Hubs.map(function(Hubs, index){
-                   return <Draggable bounds="parent"><div  className="box"><p key={ index }>{Hubs}</p><button onClick={()=>{this.deleteHub()}}>delete</button></div></Draggable>;
-                 })}
+                
+                
+                  {VMs.map(name =>{
+                    return (
+                     <Draggable bounds="parent"><div className="box"><p key={name}>{name}</p><button onClick={() => {this.deleteVm(name)}}>delete</button></div></Draggable>
+                    );
+                  })}
+                  {Hubs.map(name =>{
+                    return (
+                     <Draggable bounds="parent"><div className="box"><p key={name}>{name}</p><button onClick={() => this.deleteHub(name)}>delete</button></div></Draggable>
+                    );
+                  })}
+
          
        </div>  
        
        <div className="box" style={{height:"100px",width:"1000px" }}>
        {log.map((lines)=>
         <div><p>{lines}</p>
-
+        
         </div>
         
         )}
